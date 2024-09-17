4afd29172cc089f32c106f9467acbacffe0635f5
StellaOliveiraM <stellabertthiolly@live.com>
2024-09-17 17:05:54 -0300

    implementação de conversão

--- main.js
+++ main.js
@@ -1,17 +1,82 @@
 const axios = require("axios");
 const fs = require("fs");
 const FormData = require("form-data");
+const path = require("path");
+const sharp = require("sharp");
+const { PDFDocument } = require("pdf-lib");
 
 const apiKey = "YOUR_API_KEY";
-const filePath =
-  "/Users/stellaoliveira/Downloads/Blue Geometric Technology Software Company (1).png";
+const filePath = "FILE_PATH";
+const compressedFilePath = "FILE_PATH_COMPRESSED";
 const virusTotalScanUrl = "https://www.virustotal.com/vtapi/v2/file/scan";
 const virusTotalReportUrl = "https://www.virustotal.com/vtapi/v2/file/report";
 
-async function uploadFileToVirusTotal() {
+async function compressImage(inputPath, outputPath, maxSizeKB) {
+  let quality = 80;
+  let fileSizeInKB = Infinity;
+
+  while (fileSizeInKB > maxSizeKB) {
+    await sharp(inputPath).jpeg({ quality }).toFile(outputPath);
+
+    const stats = fs.statSync(outputPath);
+    fileSizeInKB = stats.size / 1024;
+
+    quality -= 5;
+    if (quality < 10) break;
+  }
+
+  console.log(
+    `Imagem comprimida para ${fileSizeInKB.toFixed(2)} KB com qualidade ${
+      quality + 5
+    }`
+  );
+}
+
+async function compressPDF(inputPath, outputPath, maxSizeKB) {
+  const existingPdfBytes = fs.readFileSync(inputPath);
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+  const pdfBytes = await pdfDoc.save({ useObjectStreams: false });
+
+  fs.writeFileSync(outputPath, pdfBytes);
+
+  const stats = fs.statSync(outputPath);
+  const fileSizeInKB = stats.size / 1024;
+
+  console.log(`PDF comprimido para ${fileSizeInKB.toFixed(2)} KB`);
+}
+
+async function compressFileIfNeeded(inputPath, outputPath, maxSizeKB) {
+  const stats = fs.statSync(inputPath);
+  const fileSizeInKB = stats.size / 1024;
+
+  if (fileSizeInKB <= maxSizeKB) {
+    console.log(
+      `O arquivo já está abaixo de ${maxSizeKB} KB (${fileSizeInKB.toFixed(
+        2
+      )} KB). Não é necessário comprimir.`
+    );
+    return inputPath;
+  }
+
+  const extension = path.extname(inputPath).toLowerCase();
+  const outputFilePath = `${outputPath}${extension}`;
+
+  if ([".jpg", ".jpeg", ".png"].includes(extension)) {
+    await compressImage(inputPath, outputFilePath, maxSizeKB);
+  } else if (extension === ".pdf") {
+    await compressPDF(inputPath, outputFilePath, maxSizeKB);
+  } else {
+    console.log("Tipo de arquivo não suportado para compressão.");
+    return inputPath;
+  }
+
+  return outputFilePath;
+}
+
+async function uploadFileToVirusTotal(filePathToUpload) {
   const form = new FormData();
   form.append("apikey", apiKey);
-  form.append("file", fs.createReadStream(filePath));
+  form.append("file", fs.createReadStream(filePathToUpload));
 
   try {
     const response = await axios.post(virusTotalScanUrl, form, {
@@ -42,7 +107,6 @@ async function checkFileScanStatus(scan_id) {
 
     if (response_code === 1) {
       console.log("Análise concluída! Detecções:", positives);
-      //   console.log("Detalhes dos scans:", scans);
       return true;
     } else {
       console.log(
@@ -67,10 +131,14 @@ function formatElapsedTime(startTime, endTime) {
 
 async function runVirusTotalPOC() {
   try {
+    const filePathToUpload = await compressFileIfNeeded(
+      filePath,
+      compressedFilePath,
+      400
+    );
+
     const startTime = Date.now();
-
-    const scan_id = await uploadFileToVirusTotal();
-
+    const scan_id = await uploadFileToVirusTotal(filePathToUpload);
     let scanCompleted = false;
     while (!scanCompleted) {
       scanCompleted = await checkFileScanStatus(scan_id);
@@ -78,9 +146,7 @@ async function runVirusTotalPOC() {
         await new Promise((resolve) => setTimeout(resolve, 30000));
       }
     }
-
     const endTime = Date.now();
-
     const elapsedTime = formatElapsedTime(startTime, endTime);
     console.log(
       `Processo de verificação concluído! Tempo total: ${elapsedTime}`
